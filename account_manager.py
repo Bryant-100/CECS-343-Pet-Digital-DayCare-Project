0b7e924131cec37ba41bc6e58797f27f27c1f11e
Ly Nguyen <89878447+DottL@users.noreply.github.com>
2025-04-05 01:19:43 -0700

    Add files via upload
    - wrote necessary AccountManager class for initial portion of program
    - implemented file for tester functions
    - added additional readme for notes

---
+++ account_manager.py
@@ -0,0 +1,168 @@
+from check_input import *
+import csv, random
+
+ACC_FILENAME = "users.csv"
+
+class Account:
+    def __init__(self, username, user_id):
+        self._username = username
+        self._user_id = user_id
+
+    @property
+    def username(self):
+        return self._username
+    
+    @property
+    def user_id(self):
+        return self._user_id        
+
+class AccountManager:
+    """ Handles actions involving accounts
+    Attributes:
+        _users(Account[]): stores a list of Account objects
+    """
+    def __init__(self):
+        """ Loads in recorded accounts
+        Args:
+            self (AccountManager): the AccountManager instance to be initialized
+        Returns:
+            None
+        """
+        self._users = []
+        try:            
+            with open(ACC_FILENAME, mode="r") as file:
+                reader = csv.reader(file)            
+                for row in reader:
+                    username, user_id = row
+                    self._users.append(Account(username, user_id))
+        except FileNotFoundError: # create new file if users.csv not found
+            print("Error: users.csv file not found. Creating a new file.")
+            open(ACC_FILENAME, mode="w").close()            
+    
+    def display_accounts(self):
+        """ Display current accounts
+        Args:
+            self (AccountManager): the AccountManager instance to be accessed
+        Returns:
+            None
+        """
+        if len(self._users) != 0:
+            for index,acc in enumerate(self._users, start=1):
+                print(f"{index}. {acc.username}")
+        else:
+            print("No existing users.")
+        
+    def choose_account(self):        
+        """ Display available username and process chosen account                        
+        Args:
+            self (AccountManager): the AccountManager instance to be accessed
+        Returns:
+            the Account object chosen by the user
+        """
+        # its length is additionally used as index for Exit option
+        curr_users = len(self._users) 
+        if curr_users==0: # if no users, create new account
+            return self.create_account()
+        else:    
+            print("\nWhich account is yours?")      
+            self.display_accounts()            
+            print(f"{curr_users+ 1}. Return to Login")
+            
+            choice = get_int_range("",1, curr_users+1)
+            if choice != curr_users+1:                                    
+                return self._users[choice-1]
+            else:
+                return -1
+    
+    def create_account(self):
+        """ Create a new account for the user
+        Args:
+            self (AccountManager): the AccountManager instance to be accessed
+        Returns:
+            the newly created Account object 
+        """
+        # choose and validate username
+        username = get_username([account.username for account in self._users])
+
+        # Generate a random 5-digit ID
+        curr_ids = [account.user_id for account in self._users]
+        while True:
+            new_id = str(random.randint(10000, 99999))
+            if new_id not in curr_ids:
+                break
+        
+        # Create and store Account instance
+        new_account = Account(username, new_id)
+        self._users.append(new_account)
+        
+        # saving new account to csv
+        with open(ACC_FILENAME, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([username, new_id])        
+                
+        print(f"Account created! Welcome {username}!")
+        return new_account
+    
+    def delete_account(self):
+        users_cnt = len(self._users)
+        
+        if users_cnt != 0:
+            # displays options
+            print("Which account do you want to REMOVE?")
+            self.display_accounts()
+            print(f"{users_cnt+1}. Nevermind, send me back.")
+            
+            # process account SELECTION
+            choice = get_int_range("", 1, users_cnt+1)
+            
+            # processe account DELETION                        
+            if choice != users_cnt + 1:                
+                target_account = self._users[choice - 1]
+                
+                # Remove from internal list
+                self._users.remove(target_account)
+
+                # re-read csv to filter
+                new_rows = []
+                with open(ACC_FILENAME, mode="r", newline="") as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        if row and row[0] != target_account.username:
+                            new_rows.append(row)
+                
+                # rewrite
+                with open(ACC_FILENAME, mode="w", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerows(new_rows)
+
+                print(f"Account '{target_account.username}' removed.")           
+            else: # cancellation option selected
+                print("Deletion canceled, Going back...")                         
+        else:
+            print("There's no accounts to delete.")
+    
+    def handle_login(self):
+        """ Manages login process
+        Args:
+            self (AccountManager): the AccountManager instance to be accessed
+        Returns:
+            True if successful, False otherwise
+        """
+        have_account = get_yes_no("1. Do you have an account already? ")
+        if have_account:            
+            user = self.choose_account()
+            if user != -1:
+                print(f"Welcome {user.username}!")
+                return True
+            else: # quit was chosen
+                return False            
+        else:
+            self.create_account()
+            return True
+    
+    def handle_logout():
+        pass
+    
+    
+            
+        
---
+++ check_input.py
@@ -0,0 +1,111 @@
+def get_int(prompt):
+    """ Validate user input as an integer
+    Args:
+        prompt(str): display prompt before taking input
+    Returns:
+        user input of an integer
+    """
+    value = 0
+    valid = False
+    while not valid:
+        try:
+            value = int(input(prompt))
+            valid = True
+        except ValueError:
+            print("Invalid - input needs to be an integer.")
+    return value
+
+def get_float(prompt):
+    """ Validate user input as a Float type
+    Args:
+        prompt(str): display prompt before taking input
+    Returns:
+        value of float type
+    """
+    value = 0
+    valid = False
+    while not valid:
+        try:
+            value = float(input(prompt))
+            valid = True
+        except ValueError:
+            print("Invalid - input needs to be a decimal value.")
+    return value
+
+def get_positive_int(prompt):
+    """ Validate user input as a positive integer
+    Args:
+        prompt(str): display prompt before taking input
+    Returns:
+        user input of a positive integer
+    """
+    value = 0
+    valid = False
+    while not valid:
+        try:
+            value = int(input(prompt))
+            if value >= 0:
+                valid = True
+            else:
+                print("Invalid - input can't not be negative.")
+        except ValueError:
+            print("Invalid - input needs to be an integer.")
+    return value  
+
+def get_int_range(prompt, low, high):
+    """ Validate user input as an integer between the specified range (inclusive)
+    Args:
+        prompt(str): display prompt before taking input
+        low (int): lower range bound
+        high (int): higher range bound
+    Returns:
+        an integer within the specified range
+    """
+    value = 0
+    valid = False
+    while not valid:
+        try:
+            value = int(input(prompt))
+            if value >= low and value <= high:
+                valid = True
+            else:
+                print("Invalid - input is not within range of " + str(low) + "-" + str(high) + ".")
+        except ValueError:
+            print("Invalid - input needs to be an integer.")
+    return value
+
+def get_yes_no(prompt):
+    """ Validate user input for yes/no OR y/n
+    Args:
+        prompt(str): display prompt before taking input
+    Returns:
+        True for yes, False otherwise
+    """
+    valid = False
+    while not valid:
+        value = input(prompt).upper()
+        if value == "YES" or value == "Y":
+            return True
+        elif value == "NO" or value == "N":
+            return False
+        else:
+            print("Invalid - input is either 'Yes/Y' or 'No/N'")
+
+def get_username(curr_usernames):
+    """ Validate the current username
+    Args:
+        prompt(str): display prompt before taking input
+        new_user (str): the chosen username
+        curr_usernames (str[]): the list of existing usernames
+    Returns:
+        a unique username
+    """
+    username = input("Please choose a username: ")
+    valid = False
+    while username in curr_usernames:
+        print("That username is taken :(")
+        username = input("Please choose another username: ")
+    return username
+    
+    
+    
